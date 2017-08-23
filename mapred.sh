c2893162f1f3ffd78b7975684e981ecd1afab33e
Vladimir Laskov <samflanker@gmail.com>
2017-08-23 05:34:51 +0300

    mapred.sh is changed ...

--- mapred.sh
+++ mapred.sh
@@ -6,27 +6,27 @@ NUM_COLUMNS=`head -1 $1 | awk 'END {print NF}'`
 
 rm -f /tmp/testpipe*
 
-echo Stage 0
+#echo Stage 0
 
 for cpipes in `seq 1 $NUM_COLUMNS`;
 do mkfifo /tmp/testpipe_r_${cpipes}; mkfifo /tmp/testpipe_w_${cpipes};
 done;
 
-echo Stage 1
+#echo Stage 1
 
 #awk '{ for (i = 1; i <= NF; i++) { out="/tmp/testpipe_r_"i; print $i > out } }' $1 >/dev/null 2>&1 &
 (printf "{ "; for ((i=1;i<=NUM_COLUMNS;i++)); do printf "print \$$i > \"/tmp/testpipe_r_$i\"; "; done; printf "}\n") > threads.awk 
 awk -f threads.awk $1 >/dev/null 2>&1 &
 pids="$!"
 
-echo Stage 2
+#echo Stage 2
 
 for cpipes in `seq 1 $NUM_COLUMNS`;
 do ./increment.sh /tmp/testpipe_r_${cpipes} /tmp/testpipe_w_${cpipes} >/dev/null 2>&1 &
 pids="$pids $!"
 done;
 
-echo Stage 3
+#echo Stage 3
 
 pipes_w=`seq 1 ${NUM_COLUMNS} | xargs -I {} echo "/tmp/testpipe_w_{}"`
 #echo $pipes_w
@@ -35,9 +35,4 @@ rm -f threads.awk
 paste -d' ' $pipes_w
 rm -f ./threads.awk /tmp/testpipe* 
 wait $pids
-
-
-
-
-
 
