1899c0f4e39f9d23807bf143ff7f0a277d225aee
Vladimir Laskov <samflanker@gmail.com>
2017-11-15 19:06:33 +0300

    need work with dynamic channels

--- mapred.go
+++ mapred.go
@@ -1,80 +1,79 @@
-
 package main
 
 import (
 	"bufio"
 	"fmt"
 	"os"
-        "strings"
-        "strconv"
+	"strconv"
+	"strings"
 )
 
 func main() {
-    var columns int
-    var rcounter int
-    file_path := "./file_list.txt"
-    c1 := make(chan int)
-    w1 := make(chan int)
-    s1 := make(chan bool)
+	var columns int
+	var rcounter int
+	file_path := "./data/dataset1.txt"
+	//c1 := make(chan int)
+	w1 := make(chan int)
+	s1 := make(chan bool)
+        things := make(map[int](chan int))
 
-    f, _ := os.Open(file_path)
-    scanner := bufio.NewScanner(f)
-    for scanner.Scan() {
-        line := scanner.Text()
-        // Split the line on commas.
-        parts := strings.Fields(line)
-        columns = len(parts)
-        break
-    }
-    f.Close()
+	f, _ := os.Open(file_path)
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		// Split the line on commas.
+		parts := strings.Fields(line)
+		columns = len(parts)
+		break
+	}
+	f.Close()
 
-    go reader0(c1, w1, s1, file_path )
-    rcounter++
-    //go writer0(c1, w1)
-    //rcounter++
-    _ = columns
+        i := 0
+        cstuff := i
+        things[cstuff] = make(chan int)
+        //things[sstuff] = make(chan bool)
+	go func(things[cstuff] chan int, w1 chan int, s1 chan bool, file_path string) {
+		f, _ := os.Open(file_path)
+		scanner := bufio.NewScanner(f)
+		for scanner.Scan() {
+			s1 <- true
+			line := scanner.Text()
 
-    for {
-        cstate := <-s1
-        if cstate == false {
-            break
-        }
-        number := <-c1
-        fmt.Println(number)
-    }
-    
+			// Split the line on commas.
+			parts := strings.Fields(line)
+			//fmt.Println(parts[0], parts[1])
+			number, _ := strconv.Atoi(parts[1])
+			things[cstuff] <- number + 1
+		}
+		//c1 <- rand.Int()
+		f.Close()
+		s1 <- false
+		w1 <- 0
+	}(things[cstuff], w1, s1, file_path)
+	rcounter++
 
-    for ;rcounter!=0;rcounter-- {
-       _ = <-w1
-       //fmt.Println("rcounter:", rcounter) 
-    }
-}
+	//go writer0(c1, w1)
+	//rcounter++
+	_ = columns
 
+	for {
+		cstate := <-s1
+		if cstate == false {
+			break
+		}
+		number := <-things[cstuff]
+		fmt.Println(number)
+	}
 
-
-
-func reader0(c1 chan int, w1 chan int, s1 chan bool, file_path string) {
-    f, _ := os.Open(file_path)
-    scanner := bufio.NewScanner(f)
-    for scanner.Scan() {
-        s1 <- true
-        line := scanner.Text()
-
-        // Split the line on commas.
-        parts := strings.Fields(line)
-        //fmt.Println(parts[0], parts[1])
-        number, _ := strconv.Atoi(parts[1])
-        c1 <- number + 1
-    }
-    //c1 <- rand.Int()
-    f.Close()
-    s1 <- false
-    w1 <- 0
+	for ; rcounter != 0; rcounter-- {
+		<-w1
+		//fmt.Println("rcounter:", rcounter)
+	}
 }
 
 //func writer0(c1 chan int, w1 chan int) {
 //    number := <-c1
 //    fmt.Printf("%d\n", number++)
 //    w1 <- 0
-//} 
+//}
 
